06a3870a6fe4d4d1945f2d1eb625ae27308cc450
Stoyan Grigorov <grigorov.stoyan@gmail.com>
2018-04-25 23:59:33 +0300

    dynamic content demo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from  './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Lil Nigga",
+        age: "20"
+      },
+      {
+        name: "Lil Chigga",
+        age: "36"
+      },
+      {
+        name: "Lil Wigga",
+        age: "19"
+      }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log("Switch name clicked!");
+    this.setState({
+      persons: [
+        {
+          name: "Lil Bigga",
+          age: "20"
+        },
+        {
+          name: "Lil Chigga",
+          age: "45"
+        },
+        {
+          name: "Lil Shiigga",
+          age: "19"
+        }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is working</p>
+        <button onClick={this.switchNameHandler}>Switch names!</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Dank Memes</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
